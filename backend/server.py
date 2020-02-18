4266b2497109a3671d92e860cccfc72d4a7b8632
Caio Cesar <ccesar@lenovo.com>
2020-02-18 10:49:13 -0300

    wip: updated somethings, not sure this works

--- backend/server.py
+++ backend/server.py
@@ -1,61 +1,38 @@
+from services.nfs_services import NfsServices
+
 import flask
 from flask import request, jsonify
 
-# https://flask.palletsprojects.com/en/1.1.x/patterns/fileuploads/
 
 def main():
-    books = [
-        {'id': 0,
-         'title': 'A Fire Upon the Deep',
-         'author': 'Vernor Vinge',
-         'first_sentence': 'The coldsleep itself was dreamless.',
-         'year_published': '1992'},
-        {'id': 1,
-         'title': 'The Ones Who Walk Away From Omelas',
-         'author': 'Ursula K. Le Guin',
-         'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-         'published': '1973'},
-        {'id': 2,
-         'title': 'Dhalgren',
-         'author': 'Samuel R. Delany',
-         'first_sentence': 'to wound the autumnal city.',
-         'published': '1975'}
-    ]
-
     app = flask.Flask(__name__)
 
-    @app.route('/', methods=['GET'])
-    def home():
-        return '''<h1>Distant Reading Archive</h1>
-    <p>A prototype API for distant reading of science fiction novels.</p>'''
+    nfs_services = NfsServices()
 
     # A route to return all of the available entries in our catalog.
-    @app.route('/api/v1/resources/books/all', methods=['GET'])
+    @app.route('/api/nfs/all', methods=['GET'])
     def api_all():
-        return jsonify(books)
+        result = nfs_services.GetAllNfs()
 
-    @app.route('/api/v1/resources/books', methods=['GET'])
+        return jsonify(result)
+
+    @app.route('/api/nfs/single', methods=['GET'])
     def api_id():
         # Check if an ID was provided as part of the URL.
         # If ID is provided, assign it to a variable.
         # If no ID is provided, display an error in the browser.
+        id = 0
+
         if 'id' in request.args:
             id = int(request.args['id'])
         else:
             return "Error: No id field provided. Please specify an id."
 
-        # Create an empty list for our results
-        results = []
-
-        # Loop through the data and match results that fit the requested ID.
-        # IDs are unique, but other fields might return many results
-        for book in books:
-            if book['id'] == id:
-                results.append(book)
+        result = nfs_services.GetSingle(id)
 
         # Use the jsonify function from Flask to convert our list of
         # Python dictionaries to the JSON format.
-        return jsonify(results)
+        return jsonify(result)
 
     app.run()
 
